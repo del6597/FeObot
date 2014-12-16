4ac326ef9c6ead79aebe0f010177379a79798095
David Lamont <del6597@rit.edu>
2014-12-16 00:40:29 -0800

    Getting better.

--- irclib/src/IrcStream.rs
+++ irclib/src/IrcStream.rs
@@ -42,9 +42,3 @@ impl IrcStream {
     }
     
 }
-
-fn main() {
-    let mut irc = IrcStream::new("do1.stiny.org", 6667u16);
-    irc.write("NICK RustBucket\nUSER rusty 8 * :Rust Bucket\n");
-    irc.read();
-}
